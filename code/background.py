6f2bd0f3e2310d291945eab9aa987af4a692aae9
Wanderson Nogueira <wanderson.nogueira.dev@gmail.com>
2026-06-26 14:38:38 -0300

    Creating menu interation Creating ABC class Creating Factory level 1 Bg implementation: parallax

--- code/background.py
+++ code/background.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from Const import WIN_WIDTH, ENTITY_SPEED
+from code.Entity import Entity
 
-from Entity import Entity
 
-
-class Backgroud(Entity):
-    def __init__(self):
-        pass
+class Background(Entity):
+    def __init__(self, name: str, position: tuple):
+        super().__init__(name, position)
 
     def move(self, ):
-        pass
+        self.rect.centerx -= ENTITY_SPEED[self.name]
+        if self.rect.right <= 0:
+            self.rect.left = WIN_WIDTH
