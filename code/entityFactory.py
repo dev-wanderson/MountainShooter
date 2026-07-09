00fe7a02b4397cc26f041b33bf15126b43c8674f
Wanderson Nogueira <wanderson.nogueira.dev@gmail.com>
2026-07-09 14:39:55 -0300

    Creating Mediator Pattern. Enemies destruction.

--- code/entityFactory.py
+++ code/entityFactory.py
@@ -24,6 +24,6 @@ class EntityFactory:
             case 'Player2':
                 return Player('Player2', (10, WIN_HEIGHT / 2 - 30))
             case 'Enemy1':
-                return Enemy('Enemy1', (WIN_HEIGHT + 15, random.randint(40, WIN_HEIGHT - 40)))
+                return Enemy('Enemy1', (WIN_HEIGHT + 20 , random.randint(40, WIN_HEIGHT - 40)))
             case 'Enemy2':
-                return Enemy('Enemy2', (WIN_HEIGHT + 15, random.randint(40, WIN_HEIGHT - 40)))
+                return Enemy('Enemy2', (WIN_HEIGHT + 20, random.randint(40, WIN_HEIGHT - 40)))
